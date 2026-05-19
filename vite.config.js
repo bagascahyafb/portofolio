55a13f9d6e150a6856f00247125333594a4f37db
Bagas Cahya Fajar Bastian <149213682+bagascahyafb@users.noreply.github.com>
2026-05-19 17:30:36 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portofolio/", // <--- JANGAN LUPA SLASH DI DEPAN DAN BELAKANG
+  base: "/portofolio/",
+  build: {
+    outDir: "docs",
+  },
 })
